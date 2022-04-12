4e20ef3a3e4a5a0f7845c7e9b0ed0fec7947faa8
sostene <ngarukiyimanasostene@gmail.com>
2022-04-12 10:29:43 +0200

    hashing the password using bcrypt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -14,6 +15,21 @@ const userSchema = new mongoose.Schema({
     required: [true, "please enter password"],
     minlength: [6, "minimum password lenght is 6 characters"],
   },
+});
+
+// fire a function when a doc is saved in db  mongoose hook
+// userSchema.post("save", function (doc, next) {
+//   console.log("new user was created & saved", doc);
+
+//   next();
+// });
+
+// fire a function before a doc is saved to db
+// hashing the password before save to database
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model("user", userSchema);
